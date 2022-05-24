86aec1dd1ca02e7909dc08f4edfd7fde885330bc
Evgeny Bobkin <evgeny.bobkin@europace.de>
2022-05-24 23:49:30 +0200

    add esbuild and create account construct

--- bin/ledger.ts
+++ bin/ledger.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 import 'source-map-support/register'
-import * as cdk from 'aws-cdk-lib'
+import { App } from 'aws-cdk-lib'
 import { LedgerStage } from 'lib/ledger-stage'
 
 const STAGE = 'stage'
 const DEFAULT_STAGE = 'dev'
 
-const app = new cdk.App()
+const app = new App()
 
 const stageName: string = app.node.tryGetContext(STAGE)?.toLowerCase() || DEFAULT_STAGE
 
